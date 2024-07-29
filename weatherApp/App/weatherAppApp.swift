7fa33535a5279b4e00a642fe3f6cd4c6c305ab68
Saqlain Bhatti <msbhatti.434@gmail.com>
2024-07-29 14:54:57 +0500

    Project setup
    added git ignore,
    and setup project

---
+++ weatherApp/App/weatherAppApp.swift
@@ -0,0 +1,19 @@
+//
+//  weatherAppApp.swift
+//  weatherApp
+//
+//  Created by Saqlain Bhatti on 29/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }            
+        }
+    }
+}
